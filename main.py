3ffb28e9d366b88b3ba20d5e48d9caf16537ef62
ayynny <anzh.06.03@gmail.com>
2024-08-14 17:26:14 -0700

    finished

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print("--- Begin report of books/frankenstein.txt ---")
     num_words = get_num_words(text)
-<<<<<<< HEAD
-    print(f"{num_words} words found in the document\n")
-
-    chars_dict = get_chars_dict(text)
-    print("--- End report ---")
-=======
 
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document\n")
 
-    get_char_count(text)
+    get_chars_dict(text)
    
     print(f"--- End report ---")
-
-
->>>>>>> 5986da2c8394b82b5539606863bead379698ddfc
-
 
 def get_num_words(text):
     words = text.split() 
     return len(words)
-
-def sort_on(dict):
-    return dict[""]
-
 
 def get_chars_dict(text):
     chars = {}
@@ -47,30 +32,24 @@ def get_chars_dict(text):
         print(f"The {key} character was found {value} times")
     #return chars
 
-
-    
-
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-<<<<<<< HEAD
-=======
-def get_char_count(text): 
-    lowered_string = text.lower()
+# def get_char_count(text): 
+#     lowered_string = text.lower()
     
-    char_counts = {}
+#     char_counts = {}
 
-    for character in range(0,len(lowered_string)):
-        if char_counts.get(lowered_string[character]) == None:
-            char_counts.update({lowered_string[character]:1})
-        else:
-            char_counts[lowered_string[character]] += 1
->>>>>>> 5986da2c8394b82b5539606863bead379698ddfc
+#     for character in range(0,len(lowered_string)):
+#         if char_counts.get(lowered_string[character]) == None:
+#             char_counts.update({lowered_string[character]:1})
+#         else:
+#             char_counts[lowered_string[character]] += 1
+# >>>>>>> 5986da2c8394b82b5539606863bead379698ddfc
 
-    for key,val in char_counts.items():
-        if key.isalpha() == True:
-            print(f"The '{key}' character was found {val} times")
+#     for key,val in char_counts.items():
+#         if key.isalpha() == True:
+#             print(f"The '{key}' character was found {val} times")
 
 main()
